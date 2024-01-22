b465ba537e93421e39fe19a4730ec3c46292e0f2
ChernetAsmamaw <113379056+ChernetAsmamaw@users.noreply.github.com>
2024-01-22 13:15:14 +0300

    Update main.dart

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -7,27 +7,56 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Center(
-          child: Container(
-            height: 100,
-            width: 300,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-                color: Colors.red, borderRadius: BorderRadius.circular(20)),
-            child: const Text(
-              "Hello World!",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
+      title: 'Hello World',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Hello World'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title, style: TextStyle(color: Colors.white),),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
+          ],
         ),
       ),
     );
